d00ae38eaff958158397ab2e635da18228f5fd7f
Vladimir Suvorov <vladimir.suvorov@xempus.com>
2024-11-04 10:51:04 +0100

    Render Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <raylib.h>
+
+
+struct Ball: Vector2 {
+  Ball(float _x, float _y): Vector2{.x = _x, .y = _y } {}
+  Ball(Vector2 pos): Vector2(pos) {}
+};
+
 
 int main(int argc, char ** argv)
 {
+  Ball ball(10,10);
   std::cout << "Hello Pong!" << std::endl;
+  
+  SetConfigFlags(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_ALWAYS_RUN);
+  SetTargetFPS(60);
+  InitWindow(800, 600, "Pong");
+
+  while(!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return EXIT_SUCCESS;
 }
+
