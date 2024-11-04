4dffc1618731646149666381e29ed91d5eae0b20
Vladimir Suvorov <vladimir.suvorov@xempus.com>
2024-11-04 13:46:51 +0100

    add formatting

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,17 @@
 #include <raymath.h>
 
 struct Ball {
-  Ball(Vector2 _pos, Vector2 _speed, float _radius): pos(_pos), radius(_radius), speed(_speed) {}
-  
+  Ball(Vector2 _pos, Vector2 _speed, float _radius)
+      : pos(_pos), radius(_radius), speed(_speed) {}
+
   Vector2 pos;
   Vector2 speed;
   float radius;
 
-  void draw() 
-  {
-    DrawCircleV(pos, radius, WHITE);
-  }
+  void draw() { DrawCircleV(pos, radius, WHITE); }
 
-  void update()
-  {
-    if(pos.y + radius >= GetScreenHeight() || pos.y - radius <= 0) {
+  void update() {
+    if (pos.y + radius >= GetScreenHeight() || pos.y - radius <= 0) {
       speed.y *= -1;
     } else if (pos.x + radius >= GetScreenWidth() || pos.x - radius <= 0) {
       speed.x *= -1;
@@ -25,16 +22,14 @@ struct Ball {
   }
 };
 
-
-int main(int argc, char ** argv)
-{
+int main(int argc, char **argv) {
   Ball ball(Vector2{100, 100}, Vector2{5, 5}, 20);
 
   SetConfigFlags(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_ALWAYS_RUN);
   SetTargetFPS(60);
   InitWindow(1024, 800, "Pong");
 
-  while(!WindowShouldClose()) {
+  while (!WindowShouldClose()) {
     BeginDrawing();
 
     ClearBackground(BLACK);
@@ -47,4 +42,3 @@ int main(int argc, char ** argv)
 
   return 0;
 }
-
