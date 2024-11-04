c964a78ddeb5320eb88eb605e75dad5629120d89
Vladimir Suvorov <vladimir.suvorov@xempus.com>
2024-11-04 10:55:53 +0100

    Render ball

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,19 @@
 struct Ball: Vector2 {
   Ball(float _x, float _y): Vector2{.x = _x, .y = _y } {}
   Ball(Vector2 pos): Vector2(pos) {}
+
+  void draw() 
+  {
+    DrawCircleV(static_cast<Vector2>(*this), 20, WHITE);
+  }
 };
 
 
 int main(int argc, char ** argv)
 {
-  Ball ball(10,10);
+  Ball ball(100, 100);
   std::cout << "Hello Pong!" << std::endl;
-  
+
   SetConfigFlags(FLAG_WINDOW_TOPMOST | FLAG_WINDOW_ALWAYS_RUN);
   SetTargetFPS(60);
   InitWindow(800, 600, "Pong");
@@ -20,6 +25,7 @@ int main(int argc, char ** argv)
   while(!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
+    ball.draw();
     EndDrawing();
   }
 
